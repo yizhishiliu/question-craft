0dd99060bdb2793439fcea2fd91f6ec4284e4409
yizhishiliu <15519688306@163.com>
2024-08-11 13:18:50 +0800

    MBTI小程序答题页面完善

--- mbti-test-mini/src/pages/doQuestion/index.tsx
+++ mbti-test-mini/src/pages/doQuestion/index.tsx
@@ -1,35 +1,81 @@
-import {View} from "@tarojs/components";
-import {AtButton, AtRadio} from "taro-ui";
+import { View } from "@tarojs/components";
+import { AtButton, AtRadio } from "taro-ui";
 import GlobalFooter from "../../components/GlobalFooter";
 import questions from "../../data/questions.json";
 import "./index.scss";
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
 /**
  * 做题页面
  */
 export default () => {
-  const question = questions[0];
-  const questionOptions = question.options.map((option) =>{
-    return {label: `${option.key}. ${option.value}`, value: option.key};
-  });
   // 当前题目序号（从1开始）
   const [current, setCurrent] = useState<number>(1);
+  // 当前题目
+  const [currentQuestion, setCurrentQuestion] = useState(questions[0]);
+  const questionOptions = currentQuestion.options.map((option) => {
+    return { label: `${option.key}. ${option.value}`, value: option.key };
+  });
+  // 当前答案
+  const [currentAnswer, setCurrentAnswer] = useState<string>();
+  // 回答列表
+  const [answerList] = useState<string[]>([]);
+
+  // 序号变化时，切换题目
+  useEffect(() => {
+    setCurrentQuestion(questions[current - 1]);
+    setCurrentAnswer(answerList[current - 1]);
+  }, [current]);
 
   return (
     <View className="doQuestionPage">
-      <View className="at-article__h1 title">{current}、{question.title}</View>
-      <AtRadio className="options-wrapper" options={questionOptions}/>
-      <AtButton type="primary" circle className="controlBtn">
-        下一题
-      </AtButton>
-      <AtButton type="primary" circle className="controlBtn">
-        查看结果
-      </AtButton>
-      <AtButton circle className="controlBtn">
-        上一题
-      </AtButton>
-      <GlobalFooter/>
+      <View className="at-article__h2 title">
+        {current}、{currentQuestion.title}
+      </View>
+      <AtRadio
+        className="options-wrapper"
+        options={questionOptions}
+        value={currentAnswer}
+        onClick={(value) => {
+          setCurrentAnswer(value);
+          // 记录回答
+          answerList[current - 1] = value;
+        }}
+      />
+      {current < questions.length && (
+        <AtButton
+          type="primary"
+          circle
+          className="controlBtn"
+          disabled={!currentAnswer}
+          onClick={() => setCurrent(current + 1)}
+        >
+          下一题
+        </AtButton>
+      )}
+      {current == questions.length && (
+        <AtButton
+          type="primary"
+          circle
+          className="controlBtn"
+          disabled={!currentAnswer}
+          onClick={() => {
+            // TODO: 跳转到结果页面
+          }}
+        >
+          查看结果
+        </AtButton>
+      )}
+      {current > 1 && (
+        <AtButton
+          circle
+          className="controlBtn"
+          onClick={() => setCurrent(current - 1)}
+        >
+          上一题
+        </AtButton>
+      )}
+      <GlobalFooter />
     </View>
   );
 };
