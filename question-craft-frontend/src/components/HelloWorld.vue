6aa652bc948711e28e60cbc5b87887a4c9380b5b
yizhishiliu <mingdajiang5@gmail.com>
2024-08-22 22:35:43 +0800

    全局引入arco.design组件

--- question-craft-frontend/src/components/HelloWorld.vue
+++ question-craft-frontend/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <a-avatar :style="{ backgroundColor: '#14a9f8' }">Arco</a-avatar>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
