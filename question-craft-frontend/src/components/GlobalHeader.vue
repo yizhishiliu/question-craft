fd62a5656c956b13d5b622556f90a2b886f8956e
yizhishiliu <mingdajiang5@gmail.com>
2024-08-23 00:54:58 +0800

    通用路由菜单（根据路由信息，实现更通用的菜单配置）

--- question-craft-frontend/src/components/GlobalHeader.vue
+++ question-craft-frontend/src/components/GlobalHeader.vue
@@ -8,7 +8,12 @@
     >
       <!--  wrap="false" 不换行（缩小） -->
       <a-col flex="auto">
-        <a-menu mode="horizontal" theme="light" :default-selected-keys="['1']">
+        <a-menu
+          mode="horizontal"
+          theme="light"
+          :selected-keys="selectedKeys"
+          @menu-item-click="doMenuClick"
+        >
           <a-menu-item
             key="0"
             :style="{ padding: 0, marginRight: '38px' }"
@@ -19,10 +24,9 @@
               <div class="title">智编题海</div>
             </div>
           </a-menu-item>
-          <a-menu-item key="1">Home</a-menu-item>
-          <a-menu-item key="2">Solution</a-menu-item>
-          <a-menu-item key="3">Cloud Service</a-menu-item>
-          <a-menu-item key="4">Cooperation</a-menu-item>
+          <a-menu-item v-for="item in visibleMenu" :key="item.path">
+            {{ item.name }}
+          </a-menu-item>
         </a-menu>
       </a-col>
       <a-col flex="100px">
@@ -32,7 +36,35 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+// 当前选中的菜单项
+const selectedKeys = ref(["/"]);
+// 路由跳转时，更新当前选中的菜单项
+router.afterEach((to) => {
+  selectedKeys.value = [to.path];
+});
+
+// 可见菜单
+const visibleMenu = routes.filter((item) => {
+  if (item.meta?.HideInMenu) {
+    return false;
+  }
+  return true;
+});
+
+const doMenuClick = (key: string) => {
+  // 点击菜单跳转到对应页面
+  router.push({
+    path: key,
+  });
+};
+</script>
 <style scoped>
 #globalHeader {
 }
