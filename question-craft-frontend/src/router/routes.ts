c3cc3f568843e850b14bf4fdecee837854075584
yizhishiliu <mingdajiang5@gmail.com>
2024-09-01 17:07:20 +0800

    应用详情页

--- question-craft-frontend/src/router/routes.ts
+++ question-craft-frontend/src/router/routes.ts
@@ -10,12 +10,22 @@ import AdminAppPage from "@/views/admin/AdminAppPage.vue";
 import AdminQuestionPage from "@/views/admin/AdminQuestionPage.vue";
 import AdminScoringResultPage from "@/views/admin/AdminScoringResultPage.vue";
 import AdminUserAnswerPage from "@/views/admin/AdminUserAnswerPage.vue";
+import AppDetailPage from "@/views/app/AppDetailPage.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "主页",
     component: HomePage,
+  },
+  {
+    path: "/app/detail/:id",
+    name: "应用详情页",
+    props: true,
+    component: AppDetailPage,
+    meta: {
+      HideInMenu: true,
+    },
   },
   {
     path: "/noAuth",
