7ce2ead13759d396ccff6dbb527e75f8ca8e2fde
yizhishiliu <mingdajiang5@gmail.com>
2024-09-01 17:08:33 +0800

    应用详情页优化布局

--- question-craft-frontend/src/views/app/AppDetailPage.vue
+++ question-craft-frontend/src/views/app/AppDetailPage.vue
@@ -55,7 +55,7 @@ import message from "@arco-design/web-vue/es/message";
 import { useRouter } from "vue-router";
 import { dayjs } from "@arco-design/web-vue/es/_utils/date";
 import { useLoginUserStore } from "@/store/userStore";
-import { APP_SCORING_STRATEGY_MAP, APP_TYPE_MAP } from "../../constant/app";
+import { APP_SCORING_STRATEGY_MAP, APP_TYPE_MAP } from "@/constant/app";
 
 interface Props {
   id: string;
