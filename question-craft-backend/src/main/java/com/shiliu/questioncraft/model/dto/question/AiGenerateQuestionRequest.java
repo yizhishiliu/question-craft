6e3d4d8b9ca17e0da231c44bf90aa2e0b50160fe
yizhishiliu <mingdajiang5@gmail.com>
2024-09-15 23:36:38 +0800

    AI生成题目请求类

---
+++ question-craft-backend/src/main/java/com/shiliu/questioncraft/model/dto/question/AiGenerateQuestionRequest.java
@@ -0,0 +1,27 @@
+package com.shiliu.questioncraft.model.dto.question;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="https://github.com/yizhishiliu">一之十六</a>
+ * AI 生成题目请求
+ */
+public class AiGenerateQuestionRequest implements Serializable {
+
+    /**
+     * 应用id
+     */
+    private Long appId;
+
+    /**
+     * 题目数量
+     */
+    int questionNumber = 10;
+
+    /**
+     * 选项数量
+     */
+    int optionNumber = 4;
+
+    private static final long serialVersionUID = 1L;
+}
