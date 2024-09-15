aa53952d903d7d3d7e51812509bd1fa80322d192
yizhishiliu <mingdajiang5@gmail.com>
2024-09-16 00:28:36 +0800

    AI生成题目请求类(添加lomok注解)

--- question-craft-backend/src/main/java/com/shiliu/questioncraft/model/dto/question/AiGenerateQuestionRequest.java
+++ question-craft-backend/src/main/java/com/shiliu/questioncraft/model/dto/question/AiGenerateQuestionRequest.java
@@ -1,11 +1,14 @@
 package com.shiliu.questioncraft.model.dto.question;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * @author <a href="https://github.com/yizhishiliu">一之十六</a>
  * AI 生成题目请求
  */
+@Data
 public class AiGenerateQuestionRequest implements Serializable {
 
     /**
