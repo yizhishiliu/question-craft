0272a27a9318e362d66991ec484b84316a5c0a0a
yizhishiliu <mingdajiang5@gmail.com>
2024-09-23 20:35:24 +0800

    开启Redis

--- question-craft-backend/src/main/java/com/shiliu/questioncraft/MainApplication.java
+++ question-craft-backend/src/main/java/com/shiliu/questioncraft/MainApplication.java
@@ -13,7 +13,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author <a href="https://github.com/yizhishiliu">一之十六</a>
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+//@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.shiliu.questioncraft.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
