3c099a89ae48ceb7a69fe2164109119eb4d25b63
yizhishiliu <mingdajiang5@gmail.com>
2024-09-28 18:29:49 +0800

    AppAnswerCountDTO

---
+++ question-craft-backend/src/main/java/com/shiliu/questioncraft/model/dto/statistic/AppAnswerCountDTO.java
@@ -0,0 +1,16 @@
+package com.shiliu.questioncraft.model.dto.statistic;
+
+/**
+ * App 用户提交答案数统计
+ *
+ * @author <a href="https://github.com/yizhishiliu">一之十六</a>
+ */
+public class AppAnswerCountDTO {
+
+    private Long appId;
+
+    /**
+     * 用户提交答案数
+     */
+    private Long answerCount;
+}
