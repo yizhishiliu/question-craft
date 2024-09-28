0c77b8a0eceef79cb7ded3d593826515f20df727
yizhishiliu <mingdajiang5@gmail.com>
2024-09-28 18:48:57 +0800

    应用答题结果统计

--- question-craft-backend/src/main/java/com/shiliu/questioncraft/mapper/UserAnswerMapper.java
+++ question-craft-backend/src/main/java/com/shiliu/questioncraft/mapper/UserAnswerMapper.java
@@ -2,24 +2,33 @@ package com.shiliu.questioncraft.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shiliu.questioncraft.model.dto.statistic.AppAnswerCountDTO;
+import com.shiliu.questioncraft.model.dto.statistic.AppAnswerResultCountDTO;
 import com.shiliu.questioncraft.model.entity.UserAnswer;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 /**
-* @author ada
-* @description 针对表【user_answer(用户答题记录)】的数据库操作Mapper
-* @createDate 2024-08-18 12:25:34
-* @Entity com.shiliu.questioncraft.model.entity.UserAnswer
-*/
+ * @author ada
+ * @description 针对表【user_answer(用户答题记录)】的数据库操作Mapper
+ * @createDate 2024-08-18 12:25:34
+ * @Entity com.shiliu.questioncraft.model.entity.UserAnswer
+ */
 public interface UserAnswerMapper extends BaseMapper<UserAnswer> {
 
     /**
      * 应用答题数量统计
+     *
      * @return 用户答题统计列表
      */
     List<AppAnswerCountDTO> doAppAnswerCount();
+
+    /**
+     * 应用答题结果统计
+     *
+     * @return 用户答题结果统计列表
+     */
+    List<AppAnswerResultCountDTO> doAppAnswerResultCount(Long appId);
 }
 
 
